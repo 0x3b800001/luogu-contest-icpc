a0f9ccfc22f99c0f91b5a62cedc2bb5270c8cc9d
0x3b800001 <124670529+0x3b800001@users.noreply.github.com>
2023-05-01 21:21:55 +0800

    Add files via upload

---
+++ checker.cpp
@@ -0,0 +1,63 @@
+#include <sys/time.h>
+#include <time.h>
+#include <algorithm>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include "testlib.h"
+using namespace std;
+
+#define quitwa() quitf(_wa, "Wrong Answer.")
+#define quituke() quitf(_unknown, "Unknown Error.")
+#define quitac() quitp(Minute, "ok")
+int gettimeint(std::string s) {
+  assert(s.size() == 5);
+  assert(std::isdigit(s[0]));
+  assert(std::isdigit(s[1]));
+  assert(s[2] == ':');
+  assert(std::isdigit(s[3]));
+  assert(std::isdigit(s[4]));
+  int h = (s[0] - '0') * 10 + (s[1] - '0');
+  int m = (s[3] - '0') * 10 + (s[4] - '0');
+  assert(0 <= h && h < 24);
+  assert(0 <= m && m < 60);
+  return h * 60 + m;
+}
+bool compareWords(string a, string b) {
+  vector<string> va, vb;
+  stringstream sa;
+  sa << a;
+  string cur;
+  while (sa >> cur) va.push_back(cur);
+  stringstream sb;
+  sb << b;
+  while (sb >> cur) vb.push_back(cur);
+  return (va == vb);
+}
+timeval nowtime;
+int Minute;
+void GetMinute(std::string Starttime) {
+  gettimeofday(&nowtime, NULL);
+  tm* tmp = localtime(&nowtime.tv_sec);
+  const int st = gettimeint(Starttime);
+  int h = tmp->tm_hour, m = tmp->tm_min,
+      v = (h * 60 + m - st + 480 + 60 * 48) % (60 * 24);
+  Minute = v;
+}
+signed main(signed argc, char* argv[]) {
+  registerTestlibCmd(argc, argv);
+  GetMinute("00:00");
+  std::string strAnswer;
+  while (!ans.eof()) {
+    std::string j = ans.readString();
+    if (j == "" && ans.eof()) break;
+    std::string p = ouf.readString();
+    strAnswer = p;
+    if (!compareWords(j, p)) quitwa();
+  }
+  quitac();
+  return 0;
+}
---
+++ gen.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+  std::string s;
+  int t, TL, ML;
+  std::cout << "Input name (___1.in, ...): ";
+  std::getline(std::cin, s);
+  std::cout << "Input TL (ms), ML (kb), Subtask number: \n";
+  std::cin >> TL >> ML >> t;
+  std::ofstream yml("config.yml");
+  for (int i = 1; i <= t; ++i) {
+    std::cout << "Subtask " << i << ": input number l, r and score:\n";
+    int l, r, sc;
+    std::cin >> l >> r >> sc;
+    for (int j = l; j <= r; ++j) {
+      yml << s << j << ".in:\n  timeLimit: " << TL << "\n  memoryLimit: " << ML
+          << "\n  score: 1\n  subtaskId: " << i << "\n\n";
+    }
+    std::ofstream jf("Sub" + std::to_string(i) + ".txt");
+    jf << "if";
+    for (int j = l; j < r; ++j) {
+      jf << "(@status" << j << "==AC)and";
+    }
+    jf << "(@status" << r << "==AC);then\n  @total_score=" << sc
+       << ";\n  @final_status=AC;\n  @final_time=@score" << l
+       << "*60*1000;\n  @final_memory=0;\nelse\n  @total_score=0;\n  "
+          "@final_status=UNAC;\n  @final_time=0;\n  @final_memory=0;\nfi";
+    jf.close();
+  }
+  yml.close();
+  std::ofstream total("total.txt");
+  total << "if";
+  for (int i = 1; i < t; ++i) {
+    total << "(@status" << i << "==AC)and";
+  }
+  total << "(@status" << t << "==AC);then\n  @total_score=100;\n  @final_status=AC;\n  @final_time=@time1;\n  @final_memory=0;\nelse\n  @total_score=";
+  for (int i = 1; i < t; ++i) {
+    total << "@status" << i << "+";
+  }
+  total << "@status" << t << ";\n  @final_status=UNAC;\n  @final_time=0;\n  @final_memory=0;\nfi";
+  total.close();
+  return 0;
+}
