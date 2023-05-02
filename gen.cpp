c516ca026a161b1fa7594dd7c9114d284361c793
0x3b800001 <124670529+0x3b800001@users.noreply.github.com>
2023-05-02 16:04:04 +0800

    Update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -14,18 +14,18 @@ int main() {
     int l, r, sc;
     std::cin >> l >> r >> sc;
     for (int j = l; j <= r; ++j) {
-      yml << s << j << ".in:\n  timeLimit: " << TL << "\n  memoryLimit: " << ML
-          << "\n  score: 1\n  subtaskId: " << i << "\n\n";
+      yml << s << j << ".in:\n timeLimit: " << TL << "\n memoryLimit: " << ML
+          << "\n score: 1\n subtaskId: " << i << "\n\n";
     }
     std::ofstream jf("Sub" + std::to_string(i) + ".txt");
     jf << "if";
     for (int j = l; j < r; ++j) {
       jf << "(@status" << j << "==AC)and";
     }
-    jf << "(@status" << r << "==AC);then\n  @total_score=" << sc
-       << ";\n  @final_status=AC;\n  @final_time=@score" << l
-       << "*60*1000;\n  @final_memory=0;\nelse\n  @total_score=0;\n  "
-          "@final_status=UNAC;\n  @final_time=0;\n  @final_memory=0;\nfi";
+    jf << "(@status" << r << "==AC);then\n @total_score=" << sc
+       << ";\n @final_status=AC;\n @final_time=@score" << l
+       << "*60*1000;\nelse\n @total_score=0;\n @final_status=UNAC;\n "
+          "@final_time=0;\nfi\n@final_memory=0;";
     jf.close();
   }
   yml.close();
@@ -35,13 +35,13 @@ int main() {
     total << "(@status" << i << "==AC)and";
   }
   total << "(@status" << t
-        << "==AC);then\n  @total_score=100;\n  @final_status=AC;\n  "
-           "@final_time=@time1;\n  @final_memory=0;\nelse\n  @total_score=";
-  for (int i = 1; i < t; ++i) {
-    total << "@score" << i << "+";
-  }
-  total << "@status" << t
-        << ";\n  @final_status=UNAC;\n  @final_time=0;\n  @final_memory=0;\nfi";
-  total.close();
+        << "==AC);then\n @final_status=AC;\nelse\n "
+           "@final_status=UNAC;\nfi\n@final_memory=0;\n@final_time=0;\n@total_"
+           "score=";
+  for (int i = 1; i < t; ++i) total << "@score" << i << "+";
+  total << "@score" << t << ";\n";
+  for (int i = t; i >= 1; --i)
+    total << "if(@status" << i << "==AC);then\n @final_time=@time" << i
+          << ";\nfi\n";
   return 0;
 }
